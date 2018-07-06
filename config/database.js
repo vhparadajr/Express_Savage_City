a3f20feadf7a3225498468577f440c0754c16909
vhparadajr <vhparadajr@gmail.com>
2018-07-05 20:50:41 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://clone:clone11@ds127781.mlab.com:27781/savage' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
